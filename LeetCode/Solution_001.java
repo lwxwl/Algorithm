aac4b3dbc76d781980fe16b184a09cafd9a86203
lwxwl <673834671@qq.com>
2018-02-05 22:54:10 +0800

    init

---
+++ LeetCode/Solution_001.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_001 {
+
+    /*
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+    */
+
+    /*
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                   return new int[] {i, map.get(complement)};
+            }
+         }
+        return null;
+     }
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+
+
+    public static void main(String[] args) {
+        Solution_001 solution = new Solution_001();
+        int nums[] = new int[] {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+
+}
